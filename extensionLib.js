47be653c247646cf35eb384c0a257897683b63aa
ciecolopes <45433631+ciecolopes@users.noreply.github.com>
2023-12-06 03:10:14 -0300

    Update extensionLib.js

--- extensionLib.js
+++ extensionLib.js
@@ -1,19 +1,6 @@
-var minhaVariavel = "Olá, mundo!";
+const config = { carrefour: [{ url: "https://www.carrefour.com" }] };
 
-// Função simples
-function minhaFuncao() {
-  console.log("Esta é uma função da MinhaLib");
-}
 
-// Função que aceita parâmetros
-function somar(a, b) {
-  return a + b;
-}
-
-// Expondo funcionalidades publicamente
-// Isso é necessário para que outras partes do código possam acessar essas funções e variáveis
 export {
-  minhaVariavel: minhaVariavel,
-  minhaFuncao: minhaFuncao,
-  somar: somar
+  config
 };
